d80445f8977d90e246c33b7fa203decf4294b425
Amarendra Sharma <s.amarendra1995@gmail.com>
2019-10-25 12:23:10 +0530

    plans table

---
+++ database/seeds/PlansTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PlansTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        app('rinvex.subscriptions.plan')->create([
+            'name' => 'Subscribe Job Harbour',
+            'description' => 'Job alert',
+            'price' => 9.99,
+            'signup_fee' => 1.99,
+            'invoice_period' => 1,
+            'invoice_interval' => 'month',
+            'trial_period' => 15,
+            'trial_interval' => 'day',
+            'sort_order' => 1,
+            'currency' => 'USD',
+        ]);
+    }
+}
