ad785cefc5564a0882362e3c42ed2ea57f497df6
abhishek sarmah <abhisheksarmah660@gmail.com>
2019-10-29 13:11:39 +0530

    production build issue solved, build assets pushing to master

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,23 +1,24 @@
-const mix = require('laravel-mix')
-const path = require('path')
-const tailwindcss = require('tailwindcss')
+const mix = require("laravel-mix");
+const path = require("path");
+const tailwindcss = require("tailwindcss");
+const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
 
-mix
-    .js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
+mix.js("resources/js/app.js", "public/js")
+    .sass("resources/sass/app.scss", "public/css")
     .options({
         processCssUrls: false,
-        postCss: [tailwindcss('./tailwind.config.js')],
+        postCss: [tailwindcss("./tailwind.config.js")]
     })
     .webpackConfig({
-        output: { chunkFilename: 'js/[name].js?id=[chunkhash]' },
+        output: { chunkFilename: "js/[name].js?id=[chunkhash]" },
         resolve: {
             alias: {
-                vue$: 'vue/dist/vue.runtime.esm.js',
-                '@': path.resolve('resources/js'),
-            },
+                vue$: "vue/dist/vue.runtime.esm.js",
+                "@": path.resolve("resources/js")
+            }
         },
+        plugins: [new MomentLocalesPlugin()]
     })
     .babelConfig({
-        plugins: ['@babel/plugin-syntax-dynamic-import'],
-    })
+        plugins: ["@babel/plugin-syntax-dynamic-import"]
+    });
