00e125336502ab719b9be117671142e75027fa55
abhishek sarmah <abhisheksarmah660@gmail.com>
2019-11-01 08:46:53 +0530

    migrate with seeding modified

--- database/seeds/AdminUserSeeder.php
+++ database/seeds/AdminUserSeeder.php
@@ -15,6 +15,9 @@ class AdminUserSeeder extends Seeder
         User::create([
             'name' => 'Jobsharbour Admin',
             'email' => 'admin@jobsharbour.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
             'type' => 0
         ]);
     }
