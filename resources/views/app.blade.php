adb850f67ac70e39fcf9a2f876a3b2e423412b33
abhishek sarmah <abhisheksarmah660@gmail.com>
2019-11-15 15:11:24 +0530

    todio removed and freshchat added

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -46,7 +46,15 @@
     @inertia
 
     @if((env('APP_ENV') == 'production' || env('APP_ENV') == 'staging') && ! request()->is('login') && ! request()->is('register') )
-    <script src="//code.tidio.co/9hms3zkeyei88oiswndv5qiuve4deoou.js"></script>
+    <script>
+        function initFreshChat() {
+            window.fcWidget.init({
+            token: "9ca3fe40-9986-40dc-942f-6038ae919d08",
+            host: "https://wchat.freshchat.com"
+            });
+        }
+        function initialize(i,t){var e;i.getElementById(t)?initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,e.src="https://wchat.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}function initiateCall(){initialize(document,"freshchat-js-sdk")}window.addEventListener?window.addEventListener("load",initiateCall,!1):window.attachEvent("load",initiateCall,!1);
+    </script>
     @endif
 
     @if (\Request::is('/'))
